6127dc880d7fcdbb02e240d0718aca692dcfc25b
mdsolmsawangwan <msawangwan@mdsol.com>
2020-03-06 16:58:31 -0800

    messing with the clock functions

--- cloudshell/functions/clock.py
+++ cloudshell/functions/clock.py
@@ -5,40 +5,46 @@ import datetime
 __all__ = [
     'EPOCH',
     'now',
-    'utc_iso_8601',
+    'iso_8601',
+    'utc_to_local',
+    'utc_to_local_str',
+    'local_to_utc',
+    'local_to_utc_str',
     'seconds_since_epoch',
+    'convert_iso_8601_str_to_datetime',
 ]
 
 EPOCH = datetime.datetime(1970, 1, 1, 0, 0, 0)
-ISO_8601_FORMAT = '%Y-%m-%dT%H:%M:%S%z'
+LOCAL_FORMAT = '%Y-%m-%d %H:%M:%S.%f %Z%z'
+ISO_8601_FORMAT = '%Y-%m-%dT%H:%M:%S%Z'
 
 def now(utc: bool = False) -> datetime.datetime:
     """
-    helper method, get the `datetime`, now.
+    helper method, get the `datetime`, now. defaults to local time.
     """
 
     return datetime.datetime.utcnow() if utc else datetime.datetime.now()
 
 def seconds_since_epoch(from_t: datetime.datetime = None) -> float:
     """
-    if specified, `from_t` must be in ISO-8601 UTC date format.
+    if specified, `from_t` must be UTC ISO-8601.
     """
 
     return (
         (from_t if from_t else now(utc=True)) - EPOCH
     ).total_seconds()
 
-def utc_iso_8601(d: int, m: int, y: int, hour: int = 23, minute: int = 0) -> str:
+def iso_8601(d: int, m: int, y: int, hour: int = 23, minute: int = 0) -> str:
     """
     returns a date string in the format expected by all aws services. `hour` and `minute`
     are set to default values if out of bounds while `d`ay, `m`onth
     and `y`ear raise `ValueError`.
 
-    >>> utc_iso_8601(19, 3, 2020)
+    >>> iso_8601(19, 3, 2020)
     '2020-03-19T23:00:00Z'
-    >>> utc_iso_8601(19, 3, 2020, hour=6)
+    >>> iso_8601(19, 3, 2020, hour=6)
     '2020-03-19T06:00:00Z'
-    >>> utc_iso_8601(19, 3, 2020, hour=65)
+    >>> iso_8601(19, 3, 2020, hour=65)
     '2020-03-19T00:00:00Z'
     """
 
@@ -58,6 +64,41 @@ def utc_iso_8601(d: int, m: int, y: int, hour: int = 23, minute: int = 0) -> str
         raise ValueError('(d)ay must be in 1..31')
 
     return datetime.datetime(y, m, d, hour=hour, minute=minute).isoformat() + 'Z'
+
+def convert_iso_8601_str_to_datetime(t: str) -> datetime.datetime:
+    """
+    convert a date string in aws ISO 8601 format to a `datetime` object.
+    """
+
+    return datetime.datetime.strptime(t, ISO_8601_FORMAT)
+
+def utc_to_local(t: datetime.datetime) -> datetime.datetime:
+    """
+    convert a UTC `datetime` object to a local `datetime`.
+    """
+
+    return t.replace(tzinfo=datetime.timezone.utc).astimezone(tz=None)
+
+def utc_to_local_str(t: datetime.datetime) -> str:
+    """
+    convert a UTC `datetime` object to a local date string.
+    """
+
+    return utc_to_local(t).strftime(LOCAL_FORMAT)
+
+def local_to_utc(t: datetime.datetime) -> datetime.datetime:
+    """
+    convert to a local `datetime` object to a UTC `datetime`.
+    """
+
+    return t.replace(tzinfo=None).astimezone(tz=datetime.timezone.utc)
+
+def local_to_utc_str(t: datetime.datetime) -> str:
+    """
+    convert to a local `datetime` object to a UTC date string.
+    """
+
+    return local_to_utc(t).strftime(ISO_8601_FORMAT)
 
 # import time
 # import calendar
